48c91c7776472662fe98894166a8855283256a43
Rujul Matta <rujulmatta36@gmail.com>
2026-08-17 16:29:27 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int n = prices.size();
+        int minPrice = prices[0];
+
+        for (int i = 0; i < n; i++) {
+            minPrice = min(minPrice, prices[i]);
+            ans = max(ans, prices[i] - minPrice);
+        }
+
+        return ans;
+    }
+};
