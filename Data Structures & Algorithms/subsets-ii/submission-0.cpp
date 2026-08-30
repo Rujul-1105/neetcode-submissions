d28840b61aa6ef4144a79e092a436408561907ae
Rujul Matta <rujulmatta36@gmail.com>
2026-08-30 11:43:21 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    void f(int i, vector<int>& nums, vector<int>& curr, vector<vector<int>>& ans) {
+        ans.push_back(curr);
+
+        for (int k = i; k < (int)nums.size(); k++) {
+            if (k > i && nums[k] == nums[k - 1]) continue;
+
+            curr.push_back(nums[k]);
+            f(k + 1, nums, curr, ans);
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        if ((int)nums.size() == 0) return {{}};
+
+        vector<vector<int>> ans;
+        vector<int> curr;
+
+        sort(nums.begin(), nums.end());
+        f(0, nums, curr, ans);
+        return ans;
+    }
+};
